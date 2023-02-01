087ad83685acc16f4e7ab19e0504948695c70f02
Lawrence <lawrence.langen@gmail.com>
2023-02-01 09:37:48 +0100

    Polished_Solution

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -5,17 +5,17 @@ public class App {
     public static void main(String[] args) {
     
         Konto konto1=new Konto();
-        output(String.valueOf(konto1.get()*2));
+        output(String.valueOf("Konto 1: " +konto1.get()*2));
     
     output("-------------------------");
 
         Konto konto2=new Konto();
-        output(String.valueOf(konto2.get()*3));
+        output(String.valueOf("Konto 2: " + konto2.get()*3));
 
     output("-------------------------");
 
         Konto konto3=new Konto();
-        output(String.valueOf(konto3.get()*10));
+        output(String.valueOf("Konto 3: " + konto3.get()*10));
 
     output("-------------------------");
     }
